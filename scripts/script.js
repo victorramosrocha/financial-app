3a1c0d53bb3b203d2a4819b5eae8de1334730858
VictorRocha <146087933+victorramosrocha@users.noreply.github.com>
2026-01-23 18:43:14 -0300

    update menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 function clickButtonMenu() {
     const menu = document.querySelector('#burguer-menu')
     const icon = document.querySelector('#menu-button')
+    const secbal = document.querySelector('#balance')
 
     menu.classList.toggle('active')
     icon.classList.toggle('active')
+    secbal.classList.toggle('active')
 }
